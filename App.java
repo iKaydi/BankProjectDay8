b4c7f0c2aa026bd29df9c119337e620e996b23ff
iKaydi <80319750+iKaydi@users.noreply.github.com>
2021-09-20 06:45:04 +0000

    Add files via upload

---
+++ App.java
@@ -0,0 +1,9 @@
+package com.java.BankDemo;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
---
+++ WithdrawAccountMain.java
@@ -0,0 +1,23 @@
+package com.java.BankDemo;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class WithdrawAccountMain {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int accountNo, withdrawAmount;
+		System.out.println("Enter AccountNo  ");
+		accountNo = sc.nextInt();
+		System.out.println("Enter Withdraw Amount  ");
+		withdrawAmount = sc.nextInt();
+		BankDAO dao = new BankDAO();
+		try {
+			System.out.println(dao.withdrawAccount(accountNo, withdrawAmount));
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
